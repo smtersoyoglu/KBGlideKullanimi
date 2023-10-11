cfda4a1028543fc15d2132fbf3436bc9857de33c
smter <smtersoyoglu231@gmail.com>
2023-10-11 21:20:36 +0300

    Glide

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.sametersoyoglu.kbglidekullanimi"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.sametersoyoglu.kbglidekullanimi"
@@ -33,6 +33,10 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures {
+        viewBinding= true
+    }
 }
 
 dependencies {
@@ -44,4 +48,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //Glide-resim formatı
+    implementation("com.github.bumptech.glide:glide:4.13.2")
+
 }
--- app/src/main/java/com/sametersoyoglu/kbglidekullanimi/MainActivity.kt
+++ app/src/main/java/com/sametersoyoglu/kbglidekullanimi/MainActivity.kt
@@ -2,10 +2,32 @@ package com.sametersoyoglu.kbglidekullanimi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.sametersoyoglu.kbglidekullanimi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        resimGoster("kofte.png")
+
+        binding.buttonResim1.setOnClickListener {
+            resimGoster("ayran.png")
+        }
+
+        binding.buttonResim2.setOnClickListener {
+            resimGoster("su.png")
+        }
     }
+
+    fun resimGoster(resimAdi:String) {
+        val url = "http://kasimadalan.pe.hu/yemekler/resimler/$resimAdi"
+        Glide.with(this).load(url).override(500,500).into(binding.imageView)
+    }
+    //.override() boyutu belirliyoruz bunda istediğmiz boyutta tanımlama yapabiliriz.
+    //.into(binding.imageView) nerde görüntüleyeceğimizi belirtiyoruz
 }
